235043432c02fd842eb308fb79548edd02456932
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-07-04 13:41:35 -0500

    created axios get call to feedback api/feedback router. rendered data to DOM as test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
+  const [feedBackList, setFeedbackList] = useState([])
+  useEffect (() => {
+    fetchFeedback();
+  }, [])
+
+
+  const fetchFeedback = () => {
+    axios.get('/api/feedback')
+      .then((response) => {
+        console.log('GET /api/feedback success:', response.data);
+        setFeedbackList(response.data);
+      })
+      .catch((error) => {
+        console.log('Error getting feedback:', error);
+      })
+  }
+  
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <ul>
+        {feedBackList.map((feedback) => (
+          <li>{feedback.understanding}</li>
+        ))}
+      </ul>
     </div>
   );
 }
